f9189004c36dcc1964f40d5232c1a779eed96898
aziyan99 <rajaazian08@gmail.com>
2020-05-09 21:17:29 +0700

    test service

--- src/app/services/konsulin.service.ts
+++ src/app/services/konsulin.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+const baseUrl = 'http://localhost:8000/api/v1/student';
 
 @Injectable({
   providedIn: 'root'
 })
 export class KonsulinService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll() {
+    return this.http.get(baseUrl);
+  }
 }
