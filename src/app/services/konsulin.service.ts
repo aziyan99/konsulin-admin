c99319317517ebfc2fef079112efc72e59449adb
aziyan99 <rajaazian08@gmail.com>
2020-05-09 21:54:40 +0700

    test api

--- src/app/services/konsulin.service.ts
+++ src/app/services/konsulin.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 const baseUrl = 'http://localhost:8000/api/v1/student';
 
@@ -10,7 +10,13 @@ export class KonsulinService {
 
   constructor(private http: HttpClient) { }
 
+  header(httpHeader: HttpHeaders) {
+    httpHeader.append('Accept', 'Application/json');
+  }
+
   getAll() {
+    let setHeader = new HttpHeaders();
+    this.header(setHeader);
     return this.http.get(baseUrl);
   }
 }
