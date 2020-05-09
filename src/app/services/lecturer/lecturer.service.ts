81331e43215359e5fc5b07017b4acf2de6b7ec44
aziyan99 <rajaazian08@gmail.com>
2020-05-09 22:12:21 +0700

    basic config

--- src/app/services/lecturer/lecturer.service.ts
+++ src/app/services/lecturer/lecturer.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+const baseUrl = 'http://localhost:8000/api/v1/lecturer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LecturerService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll() {
+    return this.http.get(baseUrl);
+  }
 }
