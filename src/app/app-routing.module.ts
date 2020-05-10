e902bca81a1b049e8f15af8a123110964aea749e
aziyan99 <rajaazian08@gmail.com>
2020-05-10 07:45:46 +0700

    add routing to add dosen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+
 import { DosenComponent } from './dosen/dosen.component';
+import { DosenAddComponent } from 'src/app/dosen-add/dosen-add.component';
+
 import { MahasiswaComponent } from './mahasiswa/mahasiswa.component';
 import { GroupComponent } from './group/group.component';
 import { TskripsiComponent } from './tskripsi/tskripsi.component';
@@ -11,10 +14,12 @@ import { PengaturanComponent } from './pengaturan/pengaturan.component';
 import { ProfileComponent } from './profile/profile.component';
 
 
+
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'dosen', component: DosenComponent },
+  { path: 'dosen-add', component: DosenAddComponent },
   { path: 'mahasiswa', component: MahasiswaComponent },
   { path: 'group', component: GroupComponent },
   { path: 'template-skripsi', component: TskripsiComponent },
