d18a9ce20164dff83c041866dee311359ff01e97
aziyan99 <rajaazian08@gmail.com>
2020-05-10 11:27:20 +0700

    lecturer expertises half done

--- src/app/lecturer-expertises/lecturer-expertises.component.ts
+++ src/app/lecturer-expertises/lecturer-expertises.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LecturerExpertisesService } from 'src/app/services/lecturer-expertises/lecturer-expertises.service';
 
 @Component({
   selector: 'app-lecturer-expertises',
@@ -7,9 +8,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LecturerExpertisesComponent implements OnInit {
 
-  constructor() { }
+  expertises: any;
+  newExpertise = {
+    expertise_name: ''
+  };
+
+  expertise: any;
+  editExpertise = {
+    expertise_name: ''
+  };
+
+  editForm = false;
+  addForm = false;
+
+  alert = false;
+  alertMessage = '';
+
+  constructor(private service: LecturerExpertisesService) { }
 
   ngOnInit(): void {
+    this.retriveData();
   }
 
+  retriveData() {
+    this.service.getAll()
+      .subscribe(data => {
+        this.expertises = data;
+        console.log(data);
+      },
+        error => {
+          console.log(error);
+        });
+  }
+
+  saveExpertise() {
+    if (this.newExpertise.expertise_name == "") {
+      this.alert = true;
+      this.alertMessage = "Data tidak boleh kosong";
+    } else {
+      const data = {
+        expertise_name: this.newExpertise.expertise_name
+      };
+
+      this.service.create(data)
+        .subscribe(response => {
+          console.log(response);
+          this.clearForm();
+        }, error => {
+          this.alertMessage = error.error.message;
+          this.alert = true;
+          this.clearForm();
+        });
+    }
+  }
+
+  clearForm() {
+    this.alert = false;
+    this.newExpertise = {
+      expertise_name: ''
+    };
+    this.retriveData();
+  }
+
+  setUpdateForm(id) {
+    this.service.getById(id)
+      .subscribe(data => {
+        this.expertise = data;
+        console.log(data);
+      }, error => {
+        console.log(error);
+      });
+  }
 }
--- src/app/services/lecturer-expertises/lecturer-expertises.service.ts
+++ src/app/services/lecturer-expertises/lecturer-expertises.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+const baseUrl = 'http://localhost:8000/api/v1/lecturer-expertise';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LecturerExpertisesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll() {
+    return this.http.get(baseUrl);
+  }
+
+  create(data) {
+    return this.http.post(baseUrl, data);
+  }
+
+  getById(id) {
+    return this.http.get(`${baseUrl}/${id}`);
+  }
 }
+
