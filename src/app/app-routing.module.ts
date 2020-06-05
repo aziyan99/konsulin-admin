955f507d307c127b7bf02edea779032fb38c070e
aziyan99 <rajaazian08@gmail.com>
2020-06-05 22:20:51 +0700

    make dump code for testing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: '/dashboard', component: DashboardIndexComponent}
+  { path: 'dashboard', component: DashboardIndexComponent}
 
 ];
 
