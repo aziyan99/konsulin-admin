c5827c4b93293c3690e039e611c3e6016159873f
aziyan99 <rajaazian08@gmail.com>
2020-06-05 22:15:45 +0700

    cleaning old dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+/**
+ * menu component
+ */
+ import { DashboardIndexComponent } from './pages/dashboard/dashboard-index/dashboard-index.component';
+
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '/dashboard', component: DashboardIndexComponent}
+
 ];
 
 @NgModule({
