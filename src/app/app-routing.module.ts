6f786ac0b735992cea10878e86f05dbd34b6720f
aziyan99 <rajaazian08@gmail.com>
2020-06-05 22:34:52 +0700

    add lecturer route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { Routes, RouterModule } from '@angular/router';
  * menu component
  */
  import { DashboardIndexComponent } from './pages/dashboard/dashboard-index/dashboard-index.component';
+ import { LecturerIndexComponent } from './pages/lecturer/lecturer-index/lecturer-index.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardIndexComponent}
+  { path: 'dashboard', component: DashboardIndexComponent },
+  { path: 'dosen', component: LecturerIndexComponent }
 
 ];
 
