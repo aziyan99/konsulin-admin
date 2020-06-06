81cbd4c5a7c68c7425319042dd83f8af0138dd53
aziyan99 <rajaazian08@gmail.com>
2020-06-06 14:52:37 +0700

    service for handling loading between http request

--- src/app/services/loader/loader.service.ts
+++ src/app/services/loader/loader.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoaderService {
+  isLoading = new Subject<boolean>();
 
-  constructor() { }
+  show() {
+    this.isLoading.next(true);
+  }
+
+  hide() {
+    this.isLoading.next(false);
+  }
 }
