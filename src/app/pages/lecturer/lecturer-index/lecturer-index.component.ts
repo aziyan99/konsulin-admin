82b6a09a2871a61586fc0ee25f5d94db897b9008
aziyan99 <rajaazian08@gmail.com>
2020-06-06 14:50:47 +0700

    refactoring code

--- src/app/pages/lecturer/lecturer-index/lecturer-index.component.ts
+++ src/app/pages/lecturer/lecturer-index/lecturer-index.component.ts
@@ -1,32 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 /**
- * Service 
+ * Service
  */
 import { LecturerService } from 'src/app/services/lecturer/lecturer.service';
+import { LecturerExpertisesService } from 'src/app/services/lecturerExpertises/lecturer-expertises.service';
 
 @Component({
   selector: 'app-lecturer-index',
   templateUrl: './lecturer-index.component.html',
-  styleUrls: ['./lecturer-index.component.css']
+  styleUrls: ['./lecturer-index.component.css'],
 })
 export class LecturerIndexComponent implements OnInit {
-
   lecturers: any;
 
-  constructor(private service: LecturerService) { }
+  constructor(private service: LecturerService) {}
 
   ngOnInit(): void {
     this.retriveData();
   }
 
   retriveData() {
-    this.service.getAll()
-      .subscribe(res => {
+    this.service.getAll().subscribe(
+      (res) => {
         this.lecturers = res;
         console.log(this.lecturers);
-      }, error => {
+      },
+      (error) => {
         console.error();
-      });
+      }
+    );
   }
-
 }
