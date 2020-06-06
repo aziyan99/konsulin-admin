70c50af8438444885fa97848cb369a3cc91480db
aziyan99 <rajaazian08@gmail.com>
2020-06-06 14:49:45 +0700

    add routing for dosen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,19 +4,19 @@ import { Routes, RouterModule } from '@angular/router';
 /**
  * menu component
  */
- import { DashboardIndexComponent } from './pages/dashboard/dashboard-index/dashboard-index.component';
- import { LecturerIndexComponent } from './pages/lecturer/lecturer-index/lecturer-index.component';
-
+import { DashboardIndexComponent } from './pages/dashboard/dashboard-index/dashboard-index.component';
+import { LecturerIndexComponent } from './pages/lecturer/lecturer-index/lecturer-index.component';
+import { LecturerCreateComponent } from './pages/lecturer/lecturer-create/lecturer-create.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardIndexComponent },
-  { path: 'dosen', component: LecturerIndexComponent }
-
+  { path: 'dosen', component: LecturerIndexComponent },
+  { path: 'dosen/create', component: LecturerCreateComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
