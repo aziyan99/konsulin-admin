12ef73bb3b220e810c9572b703ea6d7281f41706
rajaazian <rajaazian08@gmail.com>
2020-06-07 10:28:37 +0700

    add routing for lecturer expertises

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 /**
  * menu component
  */
+import { LecturerExpertisesComponent } from './pages/other/lecturer-expertises/lecturer-expertises.component';
 import { DashboardIndexComponent } from './pages/dashboard/dashboard-index/dashboard-index.component';
 import { LecturerIndexComponent } from './pages/lecturer/lecturer-index/lecturer-index.component';
 import { LecturerCreateComponent } from './pages/lecturer/lecturer-create/lecturer-create.component';
@@ -13,6 +14,7 @@ const routes: Routes = [
   { path: 'dashboard', component: DashboardIndexComponent },
   { path: 'dosen', component: LecturerIndexComponent },
   { path: 'dosen/create', component: LecturerCreateComponent },
+  { path: 'bidangpenelitian', component: LecturerExpertisesComponent },
 ];
 
 @NgModule({
